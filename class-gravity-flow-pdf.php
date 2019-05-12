fffce7a86474b215cd40cb4b32f4fda217e525e7
Yoren Chang <yorenchang@gmail.com>
2019-05-12 21:36:46 +0800

    Apply get_users_args() changes to PDF settings

--- class-gravity-flow-pdf.php
+++ class-gravity-flow-pdf.php
@@ -148,7 +148,14 @@ if ( class_exists( 'GFForms' ) ) {
 					),
 				),
 			);
-			$settings        = array(
+
+			$total_accounts = Gravity_Flow_Common::get_total_accounts();
+			$args           = Gravity_Flow_Common::get_users_args();
+			$number         = ( isset( $args['number'] ) && $args['number'] > 0 ) ? $args['number'] : 2000;
+			/* translators: 1: Warning icon 2: Number of users displayed 3: Open link tag 4: Close link tag */
+			$description = '<span class="gf_settings_description">' . sprintf( esc_html__( '%1$s This list of users contains only the first %2$s users in your site. %3$sLearn how to remove this limit%4$s. ', 'gravityflow' ), '<i class="dashicons dashicons-warning" style="color:red;"></i> ', $number, '<a href="https://docs.gravityflow.io/article/54-gravityflowgetusersargs" target="_blank">', '</a>' ) . '</span>';
+
+			$settings = array(
 				'title'  => 'PDF',
 				'fields' => array(
 					array(
@@ -197,18 +204,20 @@ if ( class_exists( 'GFForms' ) ) {
 						),
 					),
 					array(
-						'id'       => 'workflow_notification_users',
-						'name'     => 'workflow_notification_users[]',
-						'label'    => __( 'Select User', 'gravityflowpdf' ),
-						'size'     => '8',
-						'multiple' => 'multiple',
-						'type'     => 'select',
-						'choices'  => $account_choices,
+						'id'          => 'workflow_notification_users',
+						'name'        => 'workflow_notification_users[]',
+						'label'       => __( 'Select User', 'gravityflowpdf' ),
+						'size'        => '8',
+						'multiple'    => 'multiple',
+						'type'        => 'select',
+						'choices'     => $account_choices,
+						'description' => ( $total_accounts > $number ) ? $description : '',
 					),
 					array(
-						'name'  => 'workflow_notification_routing',
-						'label' => __( 'Routing', 'gravityflowpdf' ),
-						'type'  => 'user_routing',
+						'name'        => 'workflow_notification_routing',
+						'label'       => __( 'Routing', 'gravityflowpdf' ),
+						'type'        => 'user_routing',
+						'description' => ( $total_accounts > $number ) ? $description : '',
 					),
 					array(
 						'name'  => 'workflow_notification_from_name',
