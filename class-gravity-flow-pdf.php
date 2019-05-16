0399608936b14ddfe597a2d5c82acd6bca6932b6
Yoren Chang <yorenchang@gmail.com>
2019-05-16 17:18:07 +0800

    Update text copy

--- class-gravity-flow-pdf.php
+++ class-gravity-flow-pdf.php
@@ -155,7 +155,7 @@ if ( class_exists( 'GFForms' ) ) {
 				$args           = Gravity_Flow_Common::get_users_args();
 				$number         = ( isset( $args['number'] ) && $args['number'] > 0 ) ? $args['number'] : 2000;
 				/* translators: 1: Warning icon 2: Number of users displayed 3: Open link tag 4: Close link tag */
-				$description = ( $total_accounts > $number ) ? '<span class="gf_settings_description">' . sprintf( esc_html__( '%1$s This list of users contains only the first %2$s users in your site. %3$sLearn how to remove this limit%4$s. ', 'gravityflow' ), '<i class="dashicons dashicons-warning" style="color:red;"></i> ', $number, '<a href="https://docs.gravityflow.io/article/54-gravityflowgetusersargs" target="_blank">', '</a>' ) . '</span>' : '';
+				$description = ( $total_accounts > $number ) ? '<span class="gf_settings_description">' . sprintf( esc_html__( '%1$s The list of users contains only the first %2$s users in your site. %3$sLearn how to remove this limit%4$s. ', 'gravityflow' ), '<i class="dashicons dashicons-warning" style="color:red;"></i> ', $number, '<a href="https://docs.gravityflow.io/article/54-gravityflowgetusersargs" target="_blank">', '</a>' ) . '</span>' : '';
 			}
 
 			$settings = array(
